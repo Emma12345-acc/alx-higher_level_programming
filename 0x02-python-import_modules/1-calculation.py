8381ad9779a16d07d148be76bc2436e5efab104c
Emma12345-acc <edesiri_amori@yahoo.com>
2023-03-14 05:36:08 -0700

    a script that imports functions from the file calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print("{} + {} = {}".format(10,5,calculator_1.add(10, 5)))
-print("{} - {} = {}".format(10,5,calculator_1.sub(10, 5)))
-print("{} * {} = {}".format(10,5,calculator_1.mul(10, 5)))
-print("{} / {} = {}".format(10,5,calculator_1.div(10, 5)))
+print("{} + {} = {}".format(10,5,add(10, 5)))
+print("{} - {} = {}".format(10,5,sub(10, 5)))
+print("{} * {} = {}".format(10,5,mul(10, 5)))
+print("{} / {} = {}".format(10,5,div(10, 5)))
