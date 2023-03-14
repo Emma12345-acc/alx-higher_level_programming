6cce2f1e76765bf355684a86634f59fc1cb3653b
Emma12345-acc <edesiri_amori@yahoo.com>
2023-03-14 05:28:11 -0700

    A program that imports functions from the file calculator_1.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1
+a = 10
+b = 5
+print("{} + {} = {}".format(10,5,calculator_1.add(10, 5)))
+print("{} - {} = {}".format(10,5,calculator_1.sub(10, 5)))
+print("{} * {} = {}".format(10,5,calculator_1.mul(10, 5)))
+print("{} / {} = {}".format(10,5,calculator_1.div(10, 5)))
